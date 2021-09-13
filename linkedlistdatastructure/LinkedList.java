8c4d9cad5fa73c38f0f4c8756bd946a4b7a4600e
madan <madan@local.com>
2021-09-13 21:01:57 +0530

    added a class to create a node

---
+++ linkedlistdatastructure/LinkedList.java
@@ -0,0 +1,18 @@
+package com.linkedlistdatastructure;
+
+public class LinkedList {
+
+	private Node head;
+	
+	private class Node{
+		int data;
+		Node next;
+		
+		Node(int data){
+			this.data = data;
+			next = null;
+		}
+	}
+	
+
+}
---
+++ linkedlistdatastructure/Main.java
@@ -0,0 +1,8 @@
+package com.linkedlistdatastructure;
+
+public class Main {
+	public static void main(String[] args) {
+		LinkedList linkedList = new LinkedList();
+		
+	}
+}
